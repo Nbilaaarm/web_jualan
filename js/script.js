831c30e6a105e074fa5f0c1122a9e0ba908d43fd
Nbilaaarm <126476570+Nbilaaarm@users.noreply.github.com>
2025-10-25 17:27:44 +0700

    tes commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// ketika di klik tidak di hamburger untuk menutup menu
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
